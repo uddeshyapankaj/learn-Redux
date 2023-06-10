63ae23f483adea61a629247f47b80840c95c087f
UDDESHYA PANKAJ <Uddeshyapankaj14@gmail.com>
2023-06-10 19:12:05 +0530

    first commit

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,38 @@
+
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment , incrementByAmount} from './counterSlice'
+
+
+const Counter = () => {
+
+    const count = useSelector((state) => state.counter.value)
+
+    let dispatch=useDispatch();
+  return (
+    <div>
+        <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(incrementByAmount(100))}
+        >
+          incrementBy100
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
